039c9d23807c736f2faa4d5e0fccd1c6c634d1e5
Shantanu Borkar <shantanu.b@arishti.in>
2024-08-26 19:39:40 +0530

    create: NoteBloc, Events and State

---
+++ lib/bloc/notes/note_events.dart
@@ -0,0 +1,37 @@
+import 'package:endernote_flutter/models/note_model.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class NoteBlocEvent extends Equatable {}
+
+class CreateNote extends NoteBlocEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class ChangeNote extends NoteBlocEvent {
+  final NoteModel newNote;
+
+  ChangeNote({required this.newNote});
+
+  @override
+  List<Object?> get props => [newNote];
+}
+
+class DeleteNote extends NoteBlocEvent {
+  final String noteId;
+
+  DeleteNote({required this.noteId});
+
+  @override
+  List<Object?> get props => [noteId];
+}
+
+class SaveNoteChanges extends NoteBlocEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class ChangeEditMode extends NoteBlocEvent {
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/bloc/notes/note_states.dart
@@ -0,0 +1,63 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:endernote_flutter/models/note_model.dart';
+import 'package:flutter/widgets.dart';
+
+class NoteBlocState extends Equatable {
+  final NoteModel? currentNote;
+  final bool isLoading;
+  final List<NoteModel> notes;
+  final bool isSaving;
+  final bool isEditing;
+  final TextEditingController? noteTextController;
+  final TextEditingController? noteTitleController;
+
+  const NoteBlocState({
+    required this.currentNote,
+    required this.isLoading,
+    required this.notes,
+    required this.isSaving,
+    required this.isEditing,
+    required this.noteTextController,
+    required this.noteTitleController,
+  });
+
+  factory NoteBlocState.initial() => const NoteBlocState(
+        isSaving: false,
+        currentNote: null,
+        isLoading: false,
+        notes: [],
+        isEditing: false,
+        noteTextController: null,
+        noteTitleController: null,
+      );
+
+  @override
+  List<Object> get props =>
+      [currentNote.toString(), isLoading, notes, isSaving, isEditing];
+
+  @override
+  String toString() {
+    return 'NoteBlocState(currentNote: $currentNote, isLoading: $isLoading, notes: $notes, isSaving: $isSaving, isEditing: $isEditing )';
+  }
+
+  NoteBlocState copyWith({
+    NoteModel? currentNote,
+    bool? isLoading,
+    List<NoteModel>? notes,
+    bool? isSaving,
+    bool? isEditing,
+    TextEditingController? noteTextController,
+    TextEditingController? noteTitleController,
+  }) {
+    return NoteBlocState(
+      currentNote: currentNote ?? this.currentNote,
+      isLoading: isLoading ?? this.isLoading,
+      notes: notes ?? this.notes,
+      isSaving: isSaving ?? this.isSaving,
+      isEditing: isEditing ?? this.isEditing,
+      noteTextController: noteTextController ?? this.noteTextController,
+      noteTitleController: noteTitleController ?? this.noteTitleController,
+    );
+  }
+}
